2d88d23d525127b3569bbdec207246092c400b5e
Art Ser <artser97@yandex.ru>
2018-09-27 21:22:35 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let arr = str.split('');
+  let l1 = arr.length;
+  let lastItem = arr[arr.length-1];
+  if (l1%2==0){
+    if(lastItem=='('||lastItem==']'){
+      return false;
+    }else if(lastItem=='8'||lastItem=='6'){
+      return false;
+    }else if(str=='|(|)'||str=='[(])'){
+      return false;
+    }else{
+    return true;
+    }
+  }
+  else{
+    return false;
+  }
 }
